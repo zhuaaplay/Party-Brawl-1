b16eff589d596ab1792c13d70afdcea6fb3a4ed2
zhuaaplay <reyxuan7@gmail.com>
2026-03-15 21:50:36 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,21 @@ const urlsToCache = [
     'index.html',
     'style.css',
     'script.js',
-    'icon.png',
-    'icon-192x192.png',
-    'icon-512x512.png'
+    'manifest.json',
+    'assets/icon/icon.png',
+    'assets/icon/icon-16x16.png',
+    'assets/icon/icon-32x32.png',
+    'assets/icon/icon-48x48.png',
+    'assets/icon/icon-72x72.png',
+    'assets/icon/icon-96x96.png',
+    'assets/icon/icon-128x128.png',
+    'assets/icon/icon-144x144.png',
+    'assets/icon/icon-152x152.png',
+    'assets/icon/icon-180x180.png',
+    'assets/icon/icon-192x192.png',
+    'assets/icon/icon-256x256.png',
+    'assets/icon/icon-384x384.png',
+    'assets/icon/icon-512x512.png'
 ];
 
 // Instalasi Service Worker
